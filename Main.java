15a977320430fc9121ef56a01ecd7db0b4e39614
OoHJoO-478 <jhe00570@gmail.com>
2026-04-02 13:31:06 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package java_ex;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int age = 20;
+		String name = "何佳";
+		
+		System.out.println("私は" + name + "です。年齢は" + age + "さいです。");
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
